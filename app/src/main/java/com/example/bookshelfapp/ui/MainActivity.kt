9edcb7171c25661cedb160a06b7cbb46423b7b00
MouzzamSiddiqui07 <mouzzam.siddiqui@zopsmart.com>
2023-10-01 11:35:24 +0530

    #1 fixed bottom nav

--- app/src/main/java/com/example/bookshelfapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/bookshelfapp/ui/MainActivity.kt
@@ -1,22 +1,21 @@
 package com.example.bookshelfapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.bookshelfapp.R
 import com.example.bookshelfapp.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    var binding: ActivityMainBinding? = null
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding?.bottomNavView?.setupWithNavController(navController)
     }
