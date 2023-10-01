e1167a77f9e3ddd84e94613fd573c0740b388781
MouzzamSiddiqui07 <mouzzam.siddiqui@zopsmart.com>
2023-10-01 10:23:56 +0530

    #1 added all fragments

---
+++ app/src/main/java/com/example/bookshelfapp/ui/features/details/BookDetailsFragment.kt
@@ -0,0 +1,15 @@
+package com.example.bookshelfapp.ui.features.details
+
+import android.os.Bundle
+import android.view.View
+import com.example.bookshelfapp.base.BaseFragment
+import com.example.bookshelfapp.databinding.FragmentBookDetailsBinding
+
+class BookDetailsFragment : BaseFragment<FragmentBookDetailsBinding>(
+    FragmentBookDetailsBinding::inflate,
+) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/bookshelfapp/ui/features/favourites/FavouritesBookFragment.kt
@@ -0,0 +1,8 @@
+package com.example.bookshelfapp.ui.features.favourites
+
+import com.example.bookshelfapp.base.BaseFragment
+import com.example.bookshelfapp.databinding.FragmentFavouritesBookBinding
+
+class FavouritesBookFragment : BaseFragment<FragmentFavouritesBookBinding>(
+    FragmentFavouritesBookBinding::inflate,
+)
