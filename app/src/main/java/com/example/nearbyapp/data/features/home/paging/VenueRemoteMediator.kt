2ee9570edce7073d35e31b261fdff0c9ab875c7b
MouzzamSiddiqui07 <mouzzam.siddiqui@zopsmart.com>
2024-01-10 20:11:34 +0530

    #removed logs

--- app/src/main/java/com/example/nearbyapp/data/features/home/paging/VenueRemoteMediator.kt
+++ app/src/main/java/com/example/nearbyapp/data/features/home/paging/VenueRemoteMediator.kt
@@ -1,6 +1,5 @@
 package com.example.nearbyapp.data.features.home.paging
 
-import android.util.Log
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
